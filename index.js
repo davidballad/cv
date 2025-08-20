dcd5e850fdc83f1c2d3fb14d8736e8afff604552
davidballad <davicho_balla@hotmail.com>
2025-08-20 01:33:12 -0500

    update frontpage

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-//jshint esversion: 6
-$(document).ready(function(){
-  $("#myPicShow").visible();
-});
-
-// $(function(){
-//   $("#myPic").click(function(e){
-//     e.preventDefault();
-//     $("#myPicShow").toggle("fast");
-//   });
-// });
-
-
-  const date = new Date().getFullYear();
-  $("p.cdate").prepend(date);
-  $("p.cdate").prepend("© ");
-
-
-$("img.disnum").on("mouseover", function(){
-  alert("hi");
+// Modern JavaScript with ES6
+$(document).ready(function() {
+  // Add current year to footer
+  const currentYear = new Date().getFullYear();
+  $(".current-year").text(currentYear);
+  
+  // Smooth scrolling for navigation links
+  $("a[href^='#']").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      const hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
+  
+  // Initialize skill progress bars with animation
+  $('.progress-bar').each(function() {
+    const width = $(this).attr('aria-valuenow') + '%';
+    $(this).css('width', '0%').animate({
+      width: width
+    }, 1000);
+  });
 });
